d880d2c707a5dab8ea7cbcc9b647d61ace0bcc2d
Josiah Workman <workman.josiah@gmail.com>
2019-01-08 14:42:52 -0700

    Comments on most lines
    Added comments for dummies like me who don't know how to code

--- dueDate.sh
+++ dueDate.sh
@@ -1,17 +1,33 @@
 #!/bin/bash
 
+#########################################################################################################
 # This Script will look to JAMF Pro to see when a laptop is to be returned to the EBIO IT office
+# For the most part this requires a bunch of overhead work, creating two extension attributes for date
+# and another as a simple counter for the grace period.
+# This is intended to run once every day when a computer is dropped into a smart group. I think that should work...
+
 # Parameters are used to define the Encrypted Sting for the API Password, Return Date, Checkout Date. 
-#apiPass="$4"
-#coDate="$5"
 
-#Get Today's date
+#########################################################################################################
+
+#- Variables -#
+
+# Get Today's date
 today=$(date +%Y-%m-%d) # - date format 2019-02-09 returned as 1549753755
-#Convert to Unix for later evaluation
+# Convert to Unix time for evaluation later in the script
 todayUnix=$(date -j -f "%F" $today +%s)
-#echo $todayUnix
 
-#- Fuctions
+# This var is the number of days the loaner computer can go past it's Due Date before it's locked
+daysLeft=7
+
+# Variables for Date and Grace Period
+# DD (Due Date) GP (Grace Period)
+#dd=$7
+#gp=$8
+
+#- Functions -#
+
+# Using Encrypted Bash Strings from brysontyrrell, thanks fam
 # Alternative format for DecryptString function
 # STRING TO PASS "U2FsdGVkX19IceMm4J5883SZLODUYcwZVFdxHqmJ5Ek="
 function DecryptString() {
@@ -35,10 +51,11 @@ function GetJamfID(){
 	curl -sku $apiUser:$apiPass -H "Accept: text/xml" $apiURL/computers/serialnumber/$(GetSN) | xmllint --xpath '/computer/general/id/text()' -
 }
 
+# Get Due Date from JAMF Pro
 function GetDueDate(){
 	# Require argument $1 for comptuer's ID
-	#echo "Grabbing Due date for computer $1"
 
+	# Command subsitiution 
 	jssDue=$(curl -sku $apiUser:$apiPass -H "Accept: text/xml" $apiURL/computers/id/$1/subset/extension_attributes | xmllint --xpath "//*[id=23]/value/text()" -)
 	# If jssDue returns a zero length string, the following command will produce a string
 	# If jssDue has a value, it will return it's value to the command line
@@ -51,11 +68,12 @@ function GetDueDate(){
 	fi
 }
 
+#- Script Logic -#
+
 # Test of function - gets the due date of of a computer 
 # To run on a computer use the code below, for testing use JSSID 57
 #GetDueDate $(GetJamfID)
 GetDueDate 57
-daysLeft=7
 # Basic logic of the script, check if the jamf data suggests that the date is overdue.
 OverDueBy=$(curl -sku $apiUser:$apiPass -H "Accept: text/xml" $apiURL/computers/id/57/subset/extension_attributes | xmllint --xpath "//*[id=40]/value/text()" -)
 if [[ $todayUnix -ge $dueDate ]]
@@ -88,6 +106,7 @@ EOF
 else
 	echo computer $(GetSN) not overdue
 	# Check that the Overdue Day Extension Attribute is set to 0
+	# update the value to 0, this would occur automatically if somone forgot to change the value before loaning a computer (if the loan date is within its scheduled date, the overdue limit is set to 0)
 	if [[ $OverDueBy != 0 ]]
 	then
 	# Create xml file to copy the data to JAMF
